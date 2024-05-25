d0a83c7cdd5648847e1c56ca5bf81b56de725b1c
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-05-25 20:57:03 +0800

    continue js

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const header = document.querySelector('.header');
 const body = document.body;
 let headerHeight = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
-  console.log('scroll ' + i);
+window.addEventListener('resize', () => {
+  headerHeight = header.offsetHeight;
 })
+
+
+window.addEventListener('scroll', () => {
+ const scrollDistance = window.scrollY;
+ if (scrollDistance > 200) {
+  header.classList.add('header_fixed');
+  body.style.paddingTop = `${ headerHeight}px`;
+ } else {
+  header.classList.remove('header_fixed');
+  body.style.paddingTop = '0';
+ }
+});
+
+const choices = document.querySelectorAll('.choices');
+choices.forEach((choice) => {
+  console.log(choice);
+  const btn = choice;
+});
+// choices__btn
+// choices__box
