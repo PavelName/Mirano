8d75217155653d8caf1da52383438fb278ee201a
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-05-26 12:55:23 +0800

    continue

--- index.js
+++ index.js
@@ -19,11 +19,43 @@ window.addEventListener('scroll', () => {
   body.style.paddingTop = '0';
  }
 });
+const  adjustElementPosition = (element) => {
+  const rect = element.getBoundingClientRect();
+  const viewportWidth = window.innerWidth;
+
+  if (rect.left < 0){
+    element.style.left = '0';
+    element.style.right = 'auto';
+    element.style.transform = 'translateX(0)';
+  } else if (rect.right > viewportWidth){
+   element.style.left = 'auto';
+   element.style.right = '0';
+   element.style.transform = 'translateX(0)';
+  } else {
+    element.style.left = '50%';
+   element.style.right = 'auto';
+   element.style.transform = 'translateX(-50%)';
+  }
+  
+  
+}
 
 const choices = document.querySelectorAll('.choices');
 choices.forEach((choice) => {
-  console.log(choice);
-  const btn = choice;
+ 
+  const btn = choice.querySelector('.choices__btn');
+  const box = choice.querySelector('.choices__box');
+
+  btn.addEventListener('click', () => {
+    box.classList.toggle('choices__box_open');
+
+    adjustElementPosition(box);
+  });
+
+  window.addEventListener('resize', () => {
+    adjustElementPosition(box);
+  });
 });
+
 // choices__btn
 // choices__box
